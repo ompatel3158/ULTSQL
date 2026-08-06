80dababb3fd8862f14a2a3bf10300f23e3827b75
ompatel3158 <ompatel3158@gmail.com>
2026-08-06 15:31:50 +0530

    fix: Restore null assertion in planner.dart

--- lib/src/engine/executor/planner.dart
+++ lib/src/engine/executor/planner.dart
@@ -769,7 +769,7 @@ class QueryPlanner {
           tableName: joinSchema.name,
           dbDirectory: dbDirectory,
         );
-        final rightIndex = BTreeIndex(cache: cache, indexPath: indexFile!, keyColumns: _getKeyColumnsCount(idx));
+        final rightIndex = BTreeIndex(cache: cache, indexPath: indexFile!, keyColumns: _getKeyColumnsCount(idx!));
         currentPlan = IndexJoinNode(
           left: currentPlan,
           rightTable: rightTableFile,
